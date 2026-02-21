d7ea375f42675ab0c1cf359e88ed4bffa19cc481
RedAlexDad <boss6852@gmail.com>
2026-02-21 14:07:16 +0300

    feat: добавить страницу деталей здания и навигацию из списка
    - Создать компонент BuildingDetailsPage с информацией о здании
    - Добавить маршрутизацию на /building/:id в App.tsx
    - Реализовать переход к деталям из карточек зданий в BuildingsPage
    - Добавить изображения для кинотеатра и карты
    
    Co-authored-by: Igor Karpov <70741515+karpovigors@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import { AuthPage } from './pages/AuthPage';
 import { BuildingsPage } from './pages/BuildingsPage';
+import { BuildingDetailsPage } from './pages/BuildingDetailsPage';
 
 function App() {
   const { pathname } = window.location;
+
+  if (pathname.startsWith('/building/')) {
+    return <BuildingDetailsPage />;
+  }
 
   if (pathname === '/buildings') {
     return <BuildingsPage />;
