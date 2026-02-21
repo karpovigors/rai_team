2284b415c320ff5c418c3e975b200a04cde71954
Igor Karpov <70741515+karpovigors@users.noreply.github.com>
2026-02-21 13:31:16 +0300

    main

---
+++ rai_project/core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.views.generic import RedirectView
+from .views import home
+from . import views
+
+
+#ссылки
+urlpatterns = [
+    path('', home, name='home'),
+    path("home/", RedirectView.as_view(url="/", permanent=False)),
+    path("theatre/", views.theatre, name="theatre"),
+    path("cinema/", views.cinema, name="cinema"),
+    path("auth/", views.auth_page, name="auth"),
+    path("add_map/", views.map_adm, name="map_adm"),
+]
