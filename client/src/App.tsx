4682692314d871567ea0c82f85221b9a9e73702a
RedAlexDad <boss6852@gmail.com>
2026-02-21 13:27:56 +0300

    feat: добавить страницу списка зданий и реализовать базовую маршрутизацию
    - Создана новая страница BuildingsPage с карточками объектов
    - Реализована базовая маршрутизация между страницами авторизации и зданий
    - Добавлены стили для новой страницы
    - Обновлены глобальные стили для корректного отображения
    - Добавлены примеры карточек зданий с информацией и иконками

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
 import { AuthPage } from './pages/AuthPage';
+import { BuildingsPage } from './pages/BuildingsPage';
 
 function App() {
+  const { pathname } = window.location;
+
+  if (pathname === '/buildings') {
+    return <BuildingsPage />;
+  }
+
   return (
     <AuthPage />
   )
---
+++ client/src/pages/BuildingsPage/ui/BuildingsPage.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './BuildingsPage.css';
+
+const buildings = [
+  {
+    name: 'КАРО 11 Октябрь',
+    image: 'https://avatars.mds.yandex.net/get-altay/1881734/2a0000016b31d4a3311953c7416353d0c893/XXL',
+    details: [
+      'ежедневно, 10:00-02:00',
+      'ул. Новый Арбат, 24',
+      'Смоленская, Смоленская, Арбатская',
+    ],
+    icons: ['hearing-aid', 'wheelchair', 'sign-language'],
+  },
+  {
+    name: 'Театр Мимики и Жеста',
+    image: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/34/40/1f/caption.jpg?w=1200&h=-1&s=1',
+    details: ['Измайловский бул., 41'],
+    icons: ['hearing-aid', 'wheelchair'],
+  },
+];
+
+export const BuildingsPage: React.FC = () => {
+  return (
+    <div className="buildings-page">
+      <header className="buildings-header">
+        <h1>Информационно-навигационная платформа для людей с нарушением слуха</h1>
+        <div className="header-right">
+          <button className="map-button">Карта</button>
+          <div className="profile-icon"></div>
+        </div>
+      </header>
+      <main className="buildings-main">
+        <div className="search-container">
+          <input type="text" placeholder="Поиск" className="search-input" />
+        </div>
+        <div className="filter-buttons">
+          <button>Театр</button>
+          <button>Кинотеатр</button>
+          <button>Музей</button>
+          <button>...</button>
+        </div>
+        <div className="buildings-list">
+          {buildings.map((building, index) => (
+            <div className="building-card" key={index}>
+              <img src={building.image} alt={building.name} />
+              <h3>{building.name}</h3>
+              <ul>
+                {building.details.map((detail, i) => (
+                  <li key={i}>{detail}</li>
+                ))}
+              </ul>
+              <div className="building-icons">
+                {/* Icons would go here */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="buildings-footer"></footer>
+    </div>
+  );
+};
